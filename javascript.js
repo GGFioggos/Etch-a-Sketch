11f415201dd158836d1e79b29c1a164a11541aad
moschidisg <mosxidismosxidis@gmail.com>
2022-08-30 17:12:37 +0300

    Add variable box dimensions

--- javascript.js
+++ javascript.js
@@ -1,22 +1,48 @@
-const height = 16;
-const width = 16;
-
-
+let dimension = 16;
 
 const paintContainer = document.querySelector(".paint-container");
 
-for (let i = 1; i <= height; i++) {
-    for (let j = 1; j <= width; j++) {
+createGrid(dimension);
+
+const paintBoxes = document.querySelectorAll(".paint-box");
+
+paintBoxes.forEach( function (paintbox){
+    paintbox.addEventListener('mouseover', function() {
+        if (!this.classList.contains("painted")){
+            this.classList.add("painted");
+            console.log("painted");
+        }
+    })
+})
+
+
+
+
+function changeGrid(){
+    do {
+        dimension = prompt("How many boxes do you want per row/column? (max 100)");
+    } while (dimension > 100);
+    deleteGrid(paintContainer);
+    createGrid(dimension);
+}
+
+function createGrid(dimension){
+    for (let i = 1; i <= dimension; i++) {
+        for (let j = 1; j <= dimension; j++) {
+        
+            const div = document.createElement('div');
+            div.classList.add("paint-box");
     
-        const div = document.createElement('div');
-        div.classList.add("paint-box");
-
-        div.style.gridRow = i;
-        div.style.gridColumn = j;
-
-        paintContainer.appendChild(div);
+            div.style.gridRow = i;
+            div.style.gridColumn = j;
+    
+            paintContainer.appendChild(div);
+        }
     }
 }
 
-const paintBoxes = document.querySelectorAll(".paint-box");
-console.log(paintBoxes);
+function deleteGrid(parent){
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
